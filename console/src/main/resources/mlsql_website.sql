4986eaea0a2434f474bf8160e9da37a17b1b97ed
jinghua.zhan <jinghua.zhan@kyligence.io>
2021-09-28 14:17:30 +0800

    BZ-653 # update email template

--- console/src/main/resources/mlsql_website.sql
+++ console/src/main/resources/mlsql_website.sql
@@ -31,10 +31,8 @@ CREATE TABLE IF NOT EXISTS  `email_template` (
 -- ----------------------------
 -- Records of email_template
 -- ----------------------------
-BEGIN;
-INSERT IGNORE INTO `email_template` VALUES (1, 'reset_password_confirm', 'MLSQL LAB Password Reset Instructions', '您好，\n\n我们收到您的 MLSQL Lab 密码重置请求。如果您想要重置密码，请在您的浏览器中打开以下链接：\n\n%1$s/api/user/reset/%2$s\n\n链接有效期为30分钟。\n\nThanks,\nThe MLSQL Lab Team\n\n\n');
-INSERT IGNORE INTO `email_template` VALUES (2, 'register_verify', '欢迎在线试用 MLSQL ，请验证您的邮箱', '您好，%1$s:\n     非常感谢您试用 MLSQL Lab，我们将带您体验一站式数据科学平台，在此之前，请点击下方链接验证您的邮箱。\n\n%2$s/api/user/verification/%3$s\n\n链接有效期为30分钟。\n\nThanks,\nThe MLSQL Lab Team\n\n\n');
-COMMIT;
+INSERT IGNORE INTO `email_template` VALUES (1, 'reset_password_confirm', 'MLSQL LAB Password Reset Instructions', '您好，%1$s:\n  我们收到您的 MLSQL Lab 密码重置请求。如果您想要重置密码，请在您的浏览器中打开以下链接：\n\n%2$s/api/user/reset/%3$s\n\n链接有效期为30分钟。\n\nThanks,\nThe MLSQL Lab Team\n\n\n');
+INSERT IGNORE INTO `email_template` VALUES (2, 'register_verify', '欢迎在线试用 MLSQL ，请验证您的邮箱', '您好，%1$s:\n  非常感谢您试用 MLSQL Lab，我们将带您体验一站式数据科学平台，在此之前，请点击下方链接验证您的邮箱。\n\n%2$s/api/user/verification/%3$s\n\n链接有效期为30分钟。\n\nThanks,\nThe MLSQL Lab Team\n\n\n');
 
 -- ----------------------------
 -- Table structure for user_info
@@ -52,11 +50,5 @@ CREATE TABLE IF NOT EXISTS  `user_info` (
   UNIQUE KEY `uniq_name` (`name`),
   UNIQUE KEY `uniq_email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of user_info
--- ----------------------------
-BEGIN;
-COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
